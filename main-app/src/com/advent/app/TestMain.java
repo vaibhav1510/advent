64c6fa26459f07f26855fb5f91b5f2786cbab01b
vaibhav1510 <vaibhav@chargebee.com>
2017-11-13 10:38:24 +0530

    catalina

--- main-app/src/com/advent/app/TestMain.java
+++ main-app/src/com/advent/app/TestMain.java
@@ -2,9 +2,9 @@ package com.advent.app;
 
 public class TestMain {
 
-	public static void main(String[] args) throws Exception {
-		Class.forName("com.mysql.jdbc.Driver");
-		System.out.println("test");	
-	}
+//	public static void main(String[] args) throws Exception {
+//		Class.forName("com.mysql.jdbc.Driver");
+//		System.out.println("test");	
+//	}
 
 }
